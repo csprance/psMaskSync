9f0c6023be432996a23b8340758b22028e300e71
Chris Cunningham <chris.git@cmcproductions.co.uk>
2014-05-19 16:12:59 -0300

    Update createMat.jsx

--- createMat.jsx
+++ createMat.jsx
@@ -5,18 +5,25 @@ UI();
 
 function UI() {
     var window = new Window('dialog', 'Create Material');
-    
         // NameGroup
         var nameGroup = window.add('group');
-            var nameText = nameGroup.add('statictext');
-                nameText.text = 'Name:';
+            nameGroup.add('statictext', undefined, 'Name:');
             var nameEdit = nameGroup.add('edittext');
                 nameEdit.characters = 16;
                 
         //Buttons Group
         var btnGroup = window.add('group');
             var btnOk = btnGroup.add('button', undefined, 'Ok');
-            btnOk.onClick = function() {$.writeln('Test')};
+            btnOk.onClick = function() {createMat(nameEdit.text); window.close()};
             var btnCancel = btnGroup.add('button', undefined, 'Cancel');
         window.show();
+}
+
+
+function createMat(name) {
+    var layerSets = doc.layerSets;
+    for (var i=0; i<layerSets.length; i++) {
+        var newSet = layerSets[i].layerSets.add()
+        newSet.name = name;
+    }
 }
