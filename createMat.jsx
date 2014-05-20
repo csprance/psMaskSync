8c89ddfe44afe510a1feefd3866c4cf2c95758cf
Chris Cunningham <chris.git@cmcproductions.co.uk>
2014-05-20 09:48:11 -0300

    Update createMat.jsx

--- createMat.jsx
+++ createMat.jsx
@@ -25,5 +25,19 @@ function createMat(name) {
     for (var i=0; i<layerSets.length; i++) {
         var newSet = layerSets[i].layerSets.add()
         newSet.name = name;
+        makeLayerMask('RvlA');
     }
+}
+
+
+function makeLayerMask(maskType) {
+    if( maskType == undefined) maskType = 'RvlS' ; //from selection
+    //requires a selection 'RvlS'  complete mask 'RvlA' otherThanSelection 'HdSl'
+    var desc140 = new ActionDescriptor();
+    desc140.putClass( charIDToTypeID('Nw  '), charIDToTypeID('Chnl') );
+    var ref51 = new ActionReference();
+    ref51.putEnumerated( charIDToTypeID('Chnl'), charIDToTypeID('Chnl'), charIDToTypeID('Msk ') );
+    desc140.putReference( charIDToTypeID('At  '), ref51 );
+    desc140.putEnumerated( charIDToTypeID('Usng'), charIDToTypeID('UsrM'), charIDToTypeID(maskType) );
+    executeAction( charIDToTypeID('Mk  '), desc140, DialogModes.NO );
 }
